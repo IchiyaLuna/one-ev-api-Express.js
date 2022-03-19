6e587576dcaf9363347fa3923cbe806377c4e07e
Ichiya Luna <akkyu0724@gmail.com>
2022-03-20 03:40:24 +0900

    :white_check_mark: Add app.js
    Main entrypoint

---
+++ src/app.js
@@ -0,0 +1,8 @@
+//express module
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => console.log("IL!"));
