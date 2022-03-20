257f102396042ff0d9d0c2ddfcdb0b28423d6ad1
Ichiya Luna <akkyu0724@gmail.com>
2022-03-20 22:57:23 +0900

    :art: Move index method to index router
    Move index method in app.js to routes/index.js

--- src/app.js
+++ src/app.js
@@ -3,33 +3,16 @@ const express = require("express");
 const app = express();
 
 //express-router
+const indexRouter = require("./routes/index");
 const accountRouter = require("./routes/account");
 const academyRouter = require("./routes/academy");
 
-//db module
-const dbModule = require("./db");
-const pool = dbModule.init();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  dbModule.open(pool, (con) => {
-    con.query("SHOW TABLE STATUS", (err, result) => {
-      if (err) {
-        console.log("DB communication failed: ", err);
-        res.status(500).json({ message: "DB communication failed" });
-      } else {
-        res.json({
-          ok: true,
-          message: "DB connection OK",
-        });
-      }
-    });
-    dbModule.close(con);
-  });
-});
 
+app.use("/", indexRouter);
 app.use("/account", accountRouter);
 app.use("/academy", academyRouter);
 
