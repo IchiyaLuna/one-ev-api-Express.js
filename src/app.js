38bb819be1f171b6767f443ae5a6de4a7a11a2e6
Ichiya Luna <akkyu0724@gmail.com>
2022-03-20 13:16:25 +0900

    :sparkles: Add API check method
    GET baseuri to check API (DB) status

--- src/app.js
+++ src/app.js
@@ -5,10 +5,30 @@ const app = express();
 //express-router
 const accountRouter = require("./routes/account");
 
+//db module
+const dbModule = require("./db");
+const pool = dbModule.init();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", indexRouter);
+app.get("/", (req, res) => {
+  dbModule.open(pool, (con) => {
+    con.query("SHOW TABLE STATUS", (err, result) => {
+      if (err) {
+        console.log("DB communication failed: ", err);
+        res.status(500).json({ message: "DB communication failed" });
+      } else {
+        res.json({
+          ok: true,
+          message: "DB connection OK",
+        });
+      }
+    });
+    con.release();
+  });
+});
+
 app.use("/account", accountRouter);
 
 app.listen(3000, () => console.log("IL!"));
