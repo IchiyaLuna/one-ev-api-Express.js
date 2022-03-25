1fa636955a07af243a003921fc682d35454813bf
Ichiya Luna <akkyu0724@gmail.com>
2022-03-26 04:24:03 +0900

    :construction: academy.js WIP
    WIP

--- src/routes/academy.js
+++ src/routes/academy.js
@@ -10,10 +10,21 @@ router.get("/", (req, res) => {
   const api_key = req.query.key;
 
   dbModule.open(pool, (con) => {
-    con.query("SELECT code FROM academy WHERE api_key=?", [api_key], function (err, result) {
+    con.query("SELECT id FROM academy WHERE api_key=?", [api_key], function (err, result) {
       if (err) {
         console.log("DB communication failed: ", err);
         res.status(500).json({ message: "DB communication failed" });
+        return;
+      } else if (!result.length) {
+        res.status(404).json({ message: "No accademy found" });
+        return;
+      } else {
+        res.json({
+          ok: true,
+          data: {
+            academy_id: result[0].id,
+          },
+        });
       }
     });
   });
@@ -21,6 +32,9 @@ router.get("/", (req, res) => {
 
 router.post("/create", (req, res) => {
   const api_key = req.query.key;
+  const name = req.query.name;
+  const tel = req.query.tel;
+  const address = req.query.address;
 
   dbModule.open(pool, (con) => {
     con.query("SELECT academy_index FROM db_config", function (err, result) {
@@ -29,8 +43,41 @@ router.post("/create", (req, res) => {
         res.status(500).json({ message: "DB communication failed" });
         return;
       } else if (!result.length) {
-        console.log(result);
+        res.status(404).json({ message: "DB not correctly set" });
         return;
+      } else {
+        let id = result[0].academy_index + 1;
+        con.query(
+          "INSERT INTO academy (id, api_key, name, tel, address) VALUES(?, ?, ?, ?, ?)",
+          [id, api_key, name, tel, address],
+          function (err, result) {
+            if (err) {
+              console.log("DB communication failed: ", err);
+              res.status(500).json({ message: "DB communication failed" });
+              return;
+            } else {
+              con.query("UPDATE db_config SET academy_index=?", id, function (err, result) {
+                if (err) {
+                  console.log("DB communication failed: ", err);
+                  res.status(500).json({ message: "DB communication failed" });
+                  return;
+                } else {
+                  res.json({
+                    ok: true,
+                    message: "",
+                    data: {
+                      api_key: api_key,
+                      id: id,
+                      name: name,
+                      tel: tel,
+                      address: address,
+                    },
+                  });
+                }
+              });
+            }
+          }
+        );
       }
     });
   });
