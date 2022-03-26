4cd1f529854cd3a9e1da835daa2d4ed150e96eba
Ichiya Luna <akkyu0724@gmail.com>
2022-03-26 19:30:51 +0900

    Update teacher.js

--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -43,7 +43,7 @@ router.get("/", (req, res) => {
 
             res.json({
               ok: true,
-              student: data,
+              teacher: data,
             });
           }
         });
@@ -87,7 +87,7 @@ router.get("/search", (req, res) => {
 
             res.json({
               ok: true,
-              student: data,
+              teacher: data,
             });
           }
         });
