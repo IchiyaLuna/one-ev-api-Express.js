c112a1d92b8714dc80ad9bc7c17e97ef13885443
Ichiya Luna <akkyu0724@gmail.com>
2022-03-26 23:39:27 +0900

    Update room.js

--- src/routes/room.js
+++ src/routes/room.js
@@ -11,7 +11,6 @@ const pool = dbModule.init();
 
 router.get("/", (req, res) => {
   const api_key = req.query.key;
-  const hall_id = req.query.hall_id;
 
   dbModule.open(pool, (con) => {
     con.query("SELECT id FROM academy WHERE api_key=?", [api_key], function (err, result) {
@@ -23,7 +22,7 @@ router.get("/", (req, res) => {
       } else {
         let academy_id = result[0].id;
         dbModule.open(pool, (con) => {
-          con.query("SELECT id, name FROM room WHERE academy_id=? AND hall_id=?", [academy_id, hall_id], function (err, result) {
+          con.query("SELECT id, name FROM room WHERE academy_id=?", [academy_id], function (err, result) {
             if (err) {
               console.log("DB communication failed: ", err);
               res.status(500).json({ message: "DB communication failed" });
@@ -105,6 +104,48 @@ router.post("/", (req, res) => {
               }
             );
           }
+        });
+      }
+    });
+  });
+});
+
+router.get("/hall", (req, res) => {
+  const api_key = req.query.key;
+  const hall_id = req.query.hall_id;
+
+  dbModule.open(pool, (con) => {
+    con.query("SELECT id FROM academy WHERE api_key=?", [api_key], function (err, result) {
+      if (err) {
+        console.log("DB communication failed: ", err);
+        res.status(500).json({ message: "DB communication failed" });
+      } else if (!result.length) {
+        res.status(404).json({ message: "No accademy found" });
+      } else {
+        let academy_id = result[0].id;
+        dbModule.open(pool, (con) => {
+          con.query("SELECT id, name FROM room WHERE academy_id=? AND hall_id=?", [academy_id, hall_id], function (err, result) {
+            if (err) {
+              console.log("DB communication failed: ", err);
+              res.status(500).json({ message: "DB communication failed" });
+            } else if (!result.length) {
+              res.status(404).json({ message: "No room data found" });
+            } else {
+              const data = [];
+
+              for (const room of result) {
+                data.push({
+                  id: room.id,
+                  name: room.name,
+                });
+              }
+
+              res.json({
+                ok: true,
+                room: data,
+              });
+            }
+          });
         });
       }
     });
