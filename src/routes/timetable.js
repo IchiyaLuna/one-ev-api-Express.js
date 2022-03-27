c6732b92048deee8d92abaa6c9af4ec5998f262b
Ichiya Luna <akkyu0724@gmail.com>
2022-03-27 14:08:10 +0900

    Update timetable.js

--- src/routes/timetable.js
+++ src/routes/timetable.js
@@ -50,4 +50,66 @@ router.get("/", (req, res) => {
   });
 });
 
+router.post("/", (req, res) => {
+  const api_key = req.query.key;
+  const weekday = req.query.weekday;
+  const time = req.query.time;
+  const room_id = req.query.room_id;
+  const class_id = req.query.class_id;
+
+  dbModule.open(pool, (con) => {
+    // Get aca id
+    con.query("SELECT id FROM academy WHERE api_key=?", [api_key], function (err, result) {
+      if (err) {
+        console.log("DB communication failed: ", err);
+        res.status(500).json({ message: "DB communication failed" });
+      } else if (!result.length) {
+        res.status(204).json({ message: "No accademy found" });
+      } else {
+        let academy_id = result[0].id;
+
+        con.query("SELECT timetable_index FROM db_config", function (err, result) {
+          if (err) {
+            console.log("DB communication failed: ", err);
+            res.status(500).json({ message: "DB communication failed" });
+          } else if (!result.length) {
+            res.status(204).json({ message: "DB not correctly set" });
+          } else {
+            let id = result[0].timetable_index + 1;
+
+            con.query(
+              "INSERT INTO timetable (id, academy_id, weekday, time, room_id, class_id) VALUES(?, ?, ?, ?, ?, ?)",
+              [id, academy_id, weekday, time, room_id, class_id],
+              function (err, result) {
+                if (err) {
+                  console.log("DB communication failed: ", err);
+                  res.status(500).json({ message: "DB communication failed" });
+                } else {
+                  con.query("UPDATE db_config SET timetable_index=?", id, function (err, result) {
+                    if (err) {
+                      console.log("DB communication failed: ", err);
+                      res.status(500).json({ message: "DB communication failed" });
+                    } else {
+                      res.json({
+                        ok: true,
+                        timetable: {
+                          id: id,
+                          weekday: weekday,
+                          time: time,
+                          room_id: room_id,
+                          class_id: class_id,
+                        },
+                      });
+                    }
+                  });
+                }
+              }
+            );
+          }
+        });
+      }
+    });
+  });
+});
+
 module.exports = router;
